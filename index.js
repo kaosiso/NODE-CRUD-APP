5070bc0107ae5c775739301d04e84d5391e8fd82
kaosiso <66251506+kaosiso@users.noreply.github.com>
2025-05-30 23:22:21 +0100

    Update index.js
    Had to correct my index.jsx

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI).then(() => {
-  console.log('✅ Connected to MongoDB');
+  console.log('Connected to MongoDB');
 }).catch(err => {
   console.error('MongoDB connection error:', err);
 });
